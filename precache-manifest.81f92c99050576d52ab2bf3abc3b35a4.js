00417e8cebb32dbfcec56527267792e3dd88e732
lianginger <jackyliang444@hotmail.com.tw>
2020-05-26 12:30:36 +0800

    Updates

---
+++ precache-manifest.81f92c99050576d52ab2bf3abc3b35a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c8db243e80fa2f67cdd0844190dfe0",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "824f464c63975bea2c21",
+    "url": "/create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "966cecb820c71e7c0a5a",
+    "url": "/create-react-app/static/js/2.c4de9bc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.c4de9bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824f464c63975bea2c21",
+    "url": "/create-react-app/static/js/main.5a950f45.chunk.js"
+  },
+  {
+    "revision": "c7fbdbec5c55f78257ee",
+    "url": "/create-react-app/static/js/runtime-main.fa55bc84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
